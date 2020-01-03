1ea5bac25ea340dfa275bbcce698ab9e92661549
Odilitime <janesmith@airmail.cc>
2020-01-03 01:50:06 +0000

    param change, style change

--- dialect.appdotnet_official.callbacks.js
+++ dialect.appdotnet_official.callbacks.js
@@ -1,50 +1,49 @@
-
 const userRoutes = require('./modules/users/users.routes')
 
 function sendresponse(json, resp) {
-  var ts=new Date().getTime();
-  var diff = ts-resp.start;
+  var ts = new Date().getTime()
+  var diff = ts-resp.start
   if (diff > 1000) {
     // this could be to do the client's connection speed
     // how because we stop the clock before we send the response...
-    console.log(resp.path+' served in '+(ts-resp.start)+'ms');
+    console.log(resp.path+' served in '+(ts-resp.start)+'ms')
   }
   if (resp.prettyPrint) {
-    json=JSON.stringify(JSON.parse(json),null,4);
+    json = JSON.stringify(JSON.parse(json),null,4)
   }
-  //resp.set('Content-Type', 'text/javascript');
-  resp.type('application/json');
-  resp.setHeader("Access-Control-Allow-Origin", "*");
-  resp.send(json);
+  //resp.set('Content-Type', 'text/javascript')
+  resp.type('application/json')
+  resp.setHeader("Access-Control-Allow-Origin", "*")
+  resp.send(json)
 }
 
 function sendObject(obj, resp) {
-  var ts=new Date().getTime();
-  var diff = ts-resp.start;
+  var ts = new Date().getTime()
+  var diff = ts-resp.start
   if (diff > 1000) {
     // this could be to do the client's connection speed
     // how because we stop the clock before we send the response...
-    console.log(resp.path+' served in '+(ts-resp.start)+'ms');
+    console.log(resp.path+' served in '+(ts-resp.start)+'ms')
   }
 
-  if (obj.meta==undefined) {
-    obj.meta={ code: 200 };
+  if (obj.meta === undefined) {
+    obj.meta = { code: 200 }
   }
 
   if (obj.meta && obj.meta.code) {
-    resp.status(obj.meta.code);
+    resp.status(obj.meta.code)
   }
 
-  resp.type('application/json');
-  resp.setHeader("Access-Control-Allow-Origin", "*");
+  resp.type('application/json')
+  resp.setHeader("Access-Control-Allow-Origin", "*")
 
-  resp.send(JSON.stringify(obj,null,resp.prettyPrint?4:null));
+  resp.send(JSON.stringify(obj, null, resp.prettyPrint ? 4 : null))
 }
 
 function ISODateString(d) {
   if (!d || !d.getUTCFullYear) {
-    //console.log('created_at is type (!date): ',d,typeof(d));
-    return d;
+    //console.log('created_at is type (!date): ',d,typeof(d))
+    return d
   }
   function pad(n){return n<10 ? '0'+n : n}
   return d.getUTCFullYear()+'-'
@@ -52,13 +51,13 @@ function ISODateString(d) {
     + pad(d.getUTCDate())+'T'
     + pad(d.getUTCHours())+':'
     + pad(d.getUTCMinutes())+':'
-    + pad(parseInt(d.getUTCSeconds()))+'Z';
+    + pad(parseInt(d.getUTCSeconds()))+'Z'
 }
 
 function formattoken(token) {
   // TODO: write me
   if (token.user) {
-    token.user=userRoutes.formatuser(token.user, token);
+    token.user = userRoutes.formatuser(token.user, token)
   }
   /*
     app: {
@@ -84,7 +83,7 @@ function formattoken(token) {
     user: userRoutes.formatuser(user, token),
     "invite_link": "https://join.app.net/from/notareallink"
   */
-  return token;
+  return token
 }
 
 
@@ -94,137 +93,137 @@ function formatpost(post, token) {
 
   // Now to cast
   if (post) {
-    post.id=''+post.id; // cast to String
-    post.num_replies=parseInt(0+post.num_replies); // cast to Number
-    post.num_reposts=parseInt(0+post.num_reposts); // cast to Number
-    post.num_stars=parseInt(0+post.num_stars); // cast to Number
-    post.machine_only=post.machine_only?true:false;
-    post.is_deleted=post.is_deleted?true:false;
-    post.thread_id=''+post.thread_id; // cast to String (Number is too big for js?)
+    post.id = '' + post.id // cast to String
+    post.num_replies = parseInt(0 + post.num_replies) // cast to Number
+    post.num_reposts = parseInt(0 + post.num_reposts) // cast to Number
+    post.num_stars = parseInt(0 + post.num_stars) // cast to Number
+    post.machine_only = post.machine_only ? true : false
+    post.is_deleted = post.is_deleted ? true : false
+    post.thread_id = ''+post.thread_id // cast to String (Number is too big for js?)
     if (post.reply_to) {
-      post.reply_to=''+post.reply_to; // cast to String (Number is too big for js?)
+      post.reply_to = ''+post.reply_to // cast to String (Number is too big for js?)
     }
     // remove microtime
-    post.created_at=ISODateString(post.created_at);
+    post.created_at = ISODateString(post.created_at)
     if (token) {
       // boolean (and what about the non-existent state?)
-      post.you_reposted=post.you_reposted?true:false;
-      post.you_starred=post.you_starred?true:false;
+      post.you_reposted = post.you_reposted ? true : false
+      post.you_starred = post.you_starred ? true : false
     }
   }
-  return post;
+  return post
 }
 
 module.exports = {
   ISODateString: ISODateString,
   sendObject: sendObject,
   'postsCallback' : function(resp, token) {
-    return function(posts, err, meta) {
-      //console.log('dialect.appdotnet_official.callback.js::postsCallback - in posts callback', posts.length, 'posts', posts);
+    return function(err, posts, meta) {
+      //console.log('dialect.appdotnet_official.callback.js::postsCallback - in posts callback', posts.length, 'posts', posts)
       for(var i in posts) {
-        var post=posts[i];
-        //console.log('dialect.appdotnet_official.callback.js::postsCallback - looking at ', post, post.id, post.created_at, post.userid);
-        posts[i]=formatpost(post, token);
+        var post = posts[i]
+        //console.log('dialect.appdotnet_official.callback.js::postsCallback - looking at ', post, post.id, post.created_at, post.userid)
+        posts[i] = formatpost(post, token)
         if (post.repost_of) {
           // this is an object...
-          post.repost_of.user=userRoutes.formatuser(post.repost_of.user, token);
-          post.repost_of=formatpost(post.repost_of, token)
+          post.repost_of.user = userRoutes.formatuser(post.repost_of.user, token)
+          post.repost_of = formatpost(post.repost_of, token)
         }
         if (typeof(post.user)=='undefined') {
-          console.log('dialect.appdotnet_official.callback.js::postsCallback - missing user for post '+i);
-          posts[i].user={};
+          console.log('dialect.appdotnet_official.callback.js::postsCallback - missing user for post '+i)
+          posts[i].user = {}
         } else {
-          posts[i].user=userRoutes.formatuser(post.user, token);
+          posts[i].user = userRoutes.formatuser(post.user, token)
         }
       }
-      //console.log('dialect.appdotnet_official.callback.js::postsCallback - sending', posts.length, 'posts');
+      //console.log('dialect.appdotnet_official.callback.js::postsCallback - sending', posts.length, 'posts')
       // meta order: min_id, code, max_id, more
-      var res={
+      var res = {
         meta: meta,
         data: posts
-      };
-      sendObject(res, resp);
+      }
+      sendObject(res, resp)
     }
   },
 
   'posts2usersCallback' : function(resp, token) {
     // posts is a hack, we're converting things like global to user lists
     // we need to not do this...
-    return function(posts, err, meta) {
-      var users=[];
+    return function(err, posts, meta) {
+      var users = []
       // if any return fucking nothing (null) kill them (don't push them)
       for(var i in posts) {
-        users.push(userRoutes.formatuser(posts[i].user, token));
+        users.push(userRoutes.formatuser(posts[i].user, token))
       }
-      //console.log('returning', users);
+      //console.log('returning', users)
       // meta order: min_id, code, max_id, more
-      var res={
+      var res = {
         meta: meta,
         data: users
-      };
-      sendObject(res, resp);
+      }
+      sendObject(res, resp)
     }
   },
   'postCallback' : function(resp, token) {
     return function(err, post, meta) {
       // console.log('postCallack post', post, 'err', err, 'meta', meta)
-      var res={
+      var res = {
         meta: { code: 200 },
         data: formatpost(post, token)
-      };
+      }
       if (post && post.user) {
-        res.data.user=userRoutes.formatuser(post.user, token);
+        res.data.user = userRoutes.formatuser(post.user, token)
       }
       if (meta) {
-        res.meta=meta;
+        res.meta = meta
       }
-      sendObject(res, resp);
+      sendObject(res, resp)
     }
   },
 
   'tokenCallback' : function(resp, token) {
-    return function(data, err, meta) {
-      err = typeof err !== 'undefined' ? err : undefined;
-      meta = typeof meta !== 'undefined' ? meta : undefined;
+    return function(err, data, meta) {
+      err = typeof err !== 'undefined' ? err : undefined
+      meta = typeof meta !== 'undefined' ? meta : undefined
       var res={
         meta: meta,
         data: formattoken(data)
-      };
-      sendObject(res, resp);
+      }
+      sendObject(res, resp)
     }
   },
 
   // what's the difference between this and post?
   'dataCallback' : function(resp) {
-    return function(data, err, meta) {
-      err = typeof err !== 'undefined' ? err : undefined;
-      meta = typeof meta !== 'undefined' ? meta : undefined;
+    return function(err, data, meta) {
+      err = typeof err !== 'undefined' ? err : undefined
+      meta = typeof meta !== 'undefined' ? meta : undefined
       var res={
         meta: meta,
         data: data
-      };
-      sendObject(res, resp);
+      }
+      sendObject(res, resp)
     }
   },
 
   'fileCallback': function(resp, token) {
-    return function(data, err, meta) {
-      console.log('fileCallback', data, 'err', err, 'meta', meta);
-      err = typeof err !== 'undefined' ? err : undefined;
-      meta = typeof meta !== 'undefined' ? meta : undefined;
+    return function(err, data, meta) {
+      console.log('fileCallback', data, 'err', err, 'meta', meta)
+      err = typeof err !== 'undefined' ? err : undefined
+      meta = typeof meta !== 'undefined' ? meta : undefined
       var res={
         meta: meta,
         data: data
-      };
-      sendObject(res, resp);
+      }
+      sendObject(res, resp)
     }
   },
 
   'oembedCallback' : function(resp) {
-    return function(oembed, err) {
+    return function(err, oembed) {
       // there's no data/meta envelope for oembed
-      //console.log('ADNO::oembed got ',oembed);
-      sendresponse(JSON.stringify(oembed), resp);
+      //console.log('ADNO::oembed got ',oembed)
+      sendresponse(JSON.stringify(oembed), resp)
     }
   },
 }
