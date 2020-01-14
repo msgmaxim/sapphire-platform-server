3bd2f1393f402b2e3b39a55c41b2d8a6e6ede451
odilitime <janesmith@airmail.cc>
2020-01-14 04:08:53 +0000

    don't double start platform, make base_url configureable, disable models json

--- test/test.js
+++ test/test.js
@@ -8,8 +8,9 @@ const ADN_SCOPES = 'stream'
 // Look for a config file
 const config_path = path.join(__dirname, '/../config.json')
 // and a model file
-const config_model_path = path.join(__dirname, '/config.models.json')
-nconf.argv().env('__').file({file: config_path}).file('model', {file: config_model_path})
+//const config_model_path = path.join(__dirname, '/config.models.json')
+nconf.argv().env('__').file({file: config_path})
+//.file('model', {file: config_model_path})
 
 const cache = require('../dataaccess.proxy-admin')
 cache.start(nconf)
@@ -21,7 +22,7 @@ cache.dispatcher = {
 }
 
 let webport = nconf.get('web:port') || 7070
-const base_url = 'http://localhost:' + webport + '/'
+const base_url = 'http://' + (nconf.get('web:listen') || '127.0.0.1') + ':' + webport + '/'
 const platform_admin_url = 'http://' + (nconf.get('admin:listen') || '127.0.0.1') + ':' + (nconf.get('admin:port') || 3000)
 console.log('platform url', base_url)
 console.log('admin    url', platform_admin_url)
@@ -114,8 +115,7 @@ async function setupTesting() {
   describe('ensureServer', async () => {
     it('make sure we have something to test', async () => {
       await ensureServer()
-      console.log('server is started')
-
+      console.log('platform ready')
     })
     // need the following in an `it` to make sure it only happens after the server is set up
     it('setting up token to use with testing', async() => {
@@ -165,13 +165,4 @@ function runIntegrationTests() {
       })
 }
 
-before(function(done) {
-  // makes it at least wait until ensureServer is done before calling done
-  async function startServers() {
-    await ensureServer()
-    console.log('platform ready')
-    done()
-  }
-  startServers()
-})
 runIntegrationTests()
