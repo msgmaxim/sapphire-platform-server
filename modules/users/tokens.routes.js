f2eb24727d49265b699a335e0a4eb94bae6d7f7f
Odilitime <janesmith@airmail.cc>
2020-06-30 02:10:12 +0000

    log on err and other cleanup

--- modules/users/tokens.routes.js
+++ modules/users/tokens.routes.js
@@ -7,36 +7,38 @@ module.exports = {
     app.get(prefix+'/token', function(req, resp) {
       // req.token convert into userid/sourceid
       //console.log('dialect.*.js::/token  - looking up usertoken', req.token)
-      if (req.token!==null && req.token!==undefined && req.token!='') {
-        // need to translate token...
-        dispatcher.getUserClientByToken(req.token, function(err, usertoken) {
-          //console.log('usertoken',usertoken)
-          //console.log('dialect.*.js::/token  - got usertoken')
-          if (usertoken==null) {
-            console.log('dialect.*.js::No valid token passed in to /token', req.token)
-            var res={
-              "meta": {
-                "code": 401,
-                "error_message": "Call requires authentication: Authentication required to fetch token."
-              }
-            }
-            resp.status(401).type('application/json').send(JSON.stringify(res))
-          } else {
-            //console.log('dialect.*.js::got token for /token { userid:',usertoken.userid,'client_id:',usertoken.client_id)
-            // FIXME: pass params
-            dispatcher.getToken(usertoken.userid, usertoken.client_id, req.apiParams, callbacks.tokenCallback(resp, req.token))
-          }
-        })
-      } else {
-        console.log('dialect.*.js::No token passed in to /token')
+      if (req.token===null || req.token===undefined || req.token==='') {
+        console.log('tokens.routes.js::/token - No token passed in')
         var res={
-          "meta": {
-            "code": 401,
-            "error_message": "Call requires authentication: Authentication required to fetch token."
+          meta: {
+            code: 401,
+            error_message: "Call requires authentication: Authentication required to fetch token."
           }
         }
         resp.status(401).type('application/json').send(JSON.stringify(res))
+        return
       }
+
+      // need to translate token...
+      dispatcher.getUserClientByToken(req.token, function(err, usertoken) {
+        if (err) console.error('tokens.routes.js::/token - getUserClientByToken err', err)
+        //console.log('usertoken',usertoken)
+        //console.log('dialect.*.js::/token  - got usertoken')
+        if (usertoken==null) {
+          console.log('tokens.routes.js::/token - Not a valid token', req.token)
+          var res={
+            meta: {
+              code: 401,
+              error_message: "Call requires authentication: Authentication required to fetch token."
+            }
+          }
+          resp.status(401).type('application/json').send(JSON.stringify(res))
+          return
+        }
+        //console.log('dialect.*.js::got token for /token { userid:',usertoken.userid,'client_id:',usertoken.client_id)
+        // FIXME: pass params
+        dispatcher.getToken(usertoken.userid, usertoken.client_id, req.apiParams, callbacks.tokenCallback(resp, req.token))
+      })
     })
   }
 }
