b57970f98907316adf45efc34db83f93a0a66ff1
Odilitime <janesmith@airmail.cc>
2020-06-30 01:59:37 +0000

    getMutes() - be able to take @usernames, logging updates

--- modules/users/mutes.controller.js
+++ modules/users/mutes.controller.js
@@ -6,36 +6,40 @@ module.exports = {
   //req.params.user_id, req.apiParams, req.usertoken, callbacks.dataCallback(resp)
   getMutes: function(userids, params, tokenObj, callback) {
     var ref = this
-    var userid = userids
-    if (userids == 'me') {
-      userid = tokenObj.userid
-    }
-    this.cache.getMutes(userid, params, function(err, mutes, meta) {
-      if (mutes === false || !mutes.length) {
-        callback([], err, meta)
-        return
-      }
-      var userMutes = []
-      for(var i in mutes) {
-        var userid = mutes[i].muteeid
-        // strip paging out params but leave includes
-        // FIXME: turn into function
-        var nParams = params
-        delete nParams.before_id
-        delete nParams.since_id
-        ref.getUser(userid, nParams, function(user, userErr, userMeta) {
-          userMutes.push(user)
-          if (mutes.length == userMutes.length) {
-            callback(userMutes, err, meta)
-          }
-        })
-      }
+    // FIXME: handle arrays?
+    this.normalizeUserID(userids, tokenObj, function(err, userid) {
+      if (err) console.error('mutes.controller.js::getMutes - normalizeUserID err', err)
+      //console.log('mutes.controller.js::getMutes - effective userid', userid)
+      ref.cache.getMutes(userid, params, function(err, mutes, meta) {
+        if (err) console.error('mutes.controller.js::getMutes - getMutes err', err)
+        if (mutes === false || !mutes.length) {
+          callback(err, [], meta)
+          return
+        }
+        var userMutes = []
+        for(var i in mutes) {
+          var userid = mutes[i].muteeid
+          // strip paging out params but leave includes
+          // FIXME: turn into function
+          var nParams = params
+          delete nParams.before_id
+          delete nParams.since_id
+          ref.getUser(userid, nParams, function(user, userErr, userMeta) {
+            if (userErr) console.error('mutes.controller.js::getMutes - getUsers err', userEr)
+            userMutes.push(user)
+            if (mutes.length == userMutes.length) {
+              callback(err, userMutes, meta)
+            }
+          })
+        }
+      })
     })
   },
   addMute: function(userid, params, tokenObj, callback) {
     var ref = this
-    console.log('dispatcher::addMute', userid, 'for', tokenObj.userid)
+    console.log('mutes.controller::addMute', userid, 'for', tokenObj.userid)
     this.cache.addMute(tokenObj.userid, userid, params, function(err, mute) {
+      if (err) console.error('mutes.controller.js::addMute - err', err)
       var nParams = params
       delete nParams.before_id
       delete nParams.since_id
@@ -44,15 +48,16 @@ module.exports = {
   },
   deleteMute: function(userid, params, tokenObj, callback) {
     var ref = this
-    console.log('dispatcher::deleteMute', userid, 'for', tokenObj.userid)
+    console.log('mutes.controller::deleteMute', userid, 'for', tokenObj.userid)
     this.cache.delMute(tokenObj.userid, userid, params, function(err, mute) {
+      if (err) console.error('mutes.controller.js::deleteMute - err', err)
       var nParams = params
       delete nParams.before_id
       delete nParams.since_id
       if (mute) {
         ref.getUser(mute.muteeid, nParams, callback)
       } else {
-        console.log('dispatcher::deleteMute - mute not found', userid, 'for', tokenObj.userid)
+        console.log('mutes.controller::deleteMute - mute not found', userid, 'for', tokenObj.userid)
         callback([], '404 mute not found')
       }
     })
