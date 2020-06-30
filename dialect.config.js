a30540e2f4c558d6d3dcb0c3b486c86b3c2638e8
Odilitime <janesmith@airmail.cc>
2020-06-30 04:07:03 +0000

    expose modules

--- dialect.config.js
+++ dialect.config.js
@@ -1,6 +1,8 @@
 /**
  * Set up defined API routes at prefix
  */
+const configUtil  = require('./lib/lib.config.js')
+
 module.exports = function(app, prefix) {
   var dispatcher = app.dispatcher;
   var nconf = app.nconf;
@@ -15,6 +17,16 @@ module.exports = function(app, prefix) {
           default: {
             max_file_size: nconf.get('limits:default:max_file_size')
           }
+        },
+        modules: {
+          "channels": configUtil.moduleEnabled('channels'),
+          "clients": configUtil.moduleEnabled('client'),
+          "files": configUtil.moduleEnabled('files'),
+          "follows": configUtil.moduleEnabled('follow'),
+          "markers": configUtil.moduleEnabled('markers'),
+          "posts": configUtil.moduleEnabled('posts'),
+          "streams": configUtil.moduleEnabled('stream'),
+          "users": configUtil.moduleEnabled('users')
         }
       }
     };
